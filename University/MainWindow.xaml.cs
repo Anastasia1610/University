f6498a8d9ed1ff08d578aaee958be1923ced57a7
пк <@DESKTOP-70U3VC3>
2023-01-23 19:00:45 +0200

    Added Chek for File Format

--- University/MainWindow.xaml.cs
+++ University/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,19 +24,20 @@ namespace University
         public MainWindow()
         {
             InitializeComponent();
-
-            Student student1 = new Student("Bob", "Sinclar", 18, "D:\\Настя\\C#\\University\\University\\Fox.jpeg");
-            Student student2 = new Student("Ann", "Smith", 17, "D:\\Настя\\C#\\University\\University\\Hummingbird.jfif");
-            Student student3 = new Student("Tim", "Brown", 17, "D:\\Настя\\C#\\University\\University\\Cat.jfif");
-            Student student4 = new Student("Jack", "Rassel", 18, "D:\\Настя\\C#\\University\\University\\penguin.jfif");
+            // Создание 4 студентов и 2 групп изначально
+            Student student1 = new Student("Bob", "Sinclar", 18, "images\\Fox.jpeg");
+            Student student2 = new Student("Ann", "Smith", 17, "images\\Hummingbird.jfif");
+            Student student3 = new Student("Tim", "Brown", 17, "images\\Cat.jfif");
+            Student student4 = new Student("Jack", "Rassel", 18, "images\\penguin.jfif");
             Group group1 = new Group("KVO-37", new List<Student> { student1, student2 });
             Group group2 = new Group("KVO-40", new List<Student> { student3, student4 });
             Groups.Items.Add(group1);
-            Groups.Items.Add(group2);   
+            Groups.Items.Add(group2);
         }
 
         private void Groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // При выборе группы - очищается ListBox Students и добавляются студенты соответствующей группы
             Students.Items.Clear();
             if (Groups.SelectedItem != null)
             {
@@ -49,24 +51,28 @@ namespace University
 
         private void Students_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if(Students.SelectedItem != null)
+            // При выборе студента отображается информация в Info + фото по умолчанию, если ссілка не введена
+            if (Students.SelectedItem != null)
             {
                 Student student = Students.SelectedItem as Student;
 
                 NameField.Text = student.Name;
                 LastnameField.Text = student.LastName;
                 AgeField.Text = (student.Age).ToString();
-                if(student.LinkPhoto != null)
+
+                if (!string.IsNullOrEmpty(student.LinkPhoto))
                 {
-                    Photo.Source = BitmapFrame.Create(new Uri(student.LinkPhoto));
-                else
-                    Photo.Source = BitmapFrame.Create(new Uri("D:\\Настя\\C#\\University\\University\\Photo.png"));
+                    Photo.Source = BitmapFrame.Create(new Uri(student.LinkPhoto, UriKind.Relative));
                 }
+                else
+                    Photo.Source = BitmapFrame.Create(new Uri("Photo.png", UriKind.Relative));
             }
+        }
 
         private void AddGroupButton_Click(object sender, RoutedEventArgs e)
         {
-            if(!string.IsNullOrEmpty(GroupNameField.Text))
+            // Добавление группы
+            if (!string.IsNullOrEmpty(GroupNameField.Text))
             {
                 Group newGroup = new Group(GroupNameField.Text);
                 Groups.Items.Add(newGroup);
@@ -76,7 +82,91 @@ namespace University
 
         private void AddStudentButton_Click(object sender, RoutedEventArgs e)
         {
+            // Добавление студента
+            // Выход, если группа предварительно не выбрана
+            if (Groups.SelectedItem == null)
+            {
+                MessageBox.Show("Choice the group", "Add Students", MessageBoxButton.OK);
+                StudentNameField.Text = "";
+                StudentLastnameField.Text = "";
+                StudentAgeField.Text = "";
+                PhotoLinkTB.Text = "";
+                return;
+            }
+            // Если строки ФИО заполнениы - добавление студента
+            if (!string.IsNullOrEmpty(StudentNameField.Text) && !string.IsNullOrEmpty(StudentLastnameField.Text))
+            {
+                // Проверка введенного возраста
+                int age = 0;
+                if (int.TryParse(StudentAgeField.Text, out int result))
+                {
+                    if (result < 12 || result > 65)
+                    {
+                        MessageBox.Show("Enter correct age", "Error", MessageBoxButton.OK);
+                        StudentAgeField.Text = "";
+                        return;
+                    }
+                    age = result;
+                }
+                else
+                {
+                    MessageBox.Show("Enter correct age", "Error", MessageBoxButton.OK);
+                    StudentAgeField.Text = "";
+                    return;
+                }
 
+
+                Student newStudent = new Student(StudentNameField.Text, StudentLastnameField.Text, age, PhotoLinkTB.Text);
+                ((Group)Groups.SelectedItem).Students.Add(newStudent);
+                Students.Items.Add(newStudent);
+                StudentNameField.Text = "";
+                StudentLastnameField.Text = "";
+                StudentAgeField.Text = "";
+                PhotoLinkTB.Text = "";
+            }
+
+            // Обновление ListBox с группами (для отображения актуального кол-ва студентов после добавления)
+            Group newGroup = Groups.SelectedItem as Group;
+            for (int i = 0; i < Groups.Items.Count; i++)
+            {
+                if (Groups.Items[i] == Groups.SelectedItem)
+                {
+                    Groups.Items.RemoveAt(i);
+                }
+            }
+            Groups.Items.Add(new Group(newGroup.Name, newGroup.Students));
+
+            // Пересоздание ListBox Students
+            for (int i = 0; i < newGroup.Students.Count; i++)
+            {
+                Students.Items.Add(newGroup.Students[i]);
+            }
+
+        }
+
+        private void StudentLinkPhotoField_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myFileDialog = new OpenFileDialog();
+
+            if (myFileDialog.ShowDialog() == true)
+            {
+                // Проверка на формат файла
+                string photoLink = myFileDialog.FileName;
+                int doteIndex = photoLink.LastIndexOf('.');
+                photoLink = photoLink.Substring(doteIndex + 1);
+
+                if (photoLink == "jpg" || photoLink == "jpeg" || photoLink == "png" || photoLink == "jfif")
+                    PhotoLinkTB.Text = myFileDialog.FileName;
+                else
+                {
+                    MessageBox.Show("Неверный формат файла", "Error", MessageBoxButton.OK);
+                    return;
+                }
+            }
+            else
+            {
+                PhotoLinkTB.Text = "";
+            }
         }
     }
 
@@ -92,12 +182,12 @@ namespace University
 
         public string Name { get; set; }
         public string LastName { get; set; }
-        public int Age { get; set; }
+        public int? Age { get; set; }
         public string? LinkPhoto { get; set; }
 
         public override string ToString()
         {
-            return $"{Name} {LastName}, age: {Age}";    
+            return $"{Name} {LastName}, age: {Age}";
         }
 
     }
